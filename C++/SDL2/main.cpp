b33b4e1e8db4d2a6b2020950c6f7fa87eafc53cd
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-03 00:22:16 +0300

    Update main.cpp

--- C++/SDL2/main.cpp
+++ C++/SDL2/main.cpp
@@ -1,30 +1,75 @@
-#include<iostream>
+#pragma once
 #include<SDL2/SDL.h>
+#include<cmath> 
 
-SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Event event;
-bool isRunning = false;
 
-int main(int argc, char** argv)
-{
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window,0,-1);
-    isRunning = true;
-    while(isRunning) 
+namespace algorithm {
+    void drawCircleBresenham(SDL_Renderer* renderer, int centerX, int centerY, int radius) 
     {
-        while(SDL_PollEvent(&event)) 
-        {
-            if(event.type == SDL_QUIT)
-                isRunning= false;
+        int x = radius;
+        int y = 0;
+        int p = 1 - radius;
+
+        while (x >= y) {
+            SDL_RenderDrawPoint(renderer, centerX + x, centerY + y); // 1. bölge
+            SDL_RenderDrawPoint(renderer, centerX - x, centerY + y); // 2. bölge
+            SDL_RenderDrawPoint(renderer, centerX + x, centerY - y); // 3. bölge
+            SDL_RenderDrawPoint(renderer, centerX - x, centerY - y); // 4. bölge
+            SDL_RenderDrawPoint(renderer, centerX + y, centerY + x); // 5. bölge
+            SDL_RenderDrawPoint(renderer, centerX - y, centerY + x); // 6. bölge
+            SDL_RenderDrawPoint(renderer, centerX + y, centerY - x); // 7. bölge
+            SDL_RenderDrawPoint(renderer, centerX - y, centerY - x); // 8. bölge
+            y++;
+
+            if (p < 0)
+                p += 2 * y + 1;
+            else {
+                x--;
+                p += 2 * (y - x) + 1;
+            }
         }
-        SDL_SetRenderDrawColor(renderer,0,0,0,255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
     }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+    void drawCirclePolar(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+        const float PI = 3.14159265;
+        for (float theta = 0.0f; theta < 2 * PI; theta += 0.01f) {
+            int x = centerX + radius * cos(theta);
+            int y = centerY + radius * sin(theta);
+            SDL_RenderDrawPoint(renderer, x, y);
+        }
+    }
+
+    void drawFilledCirclePolar(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+        for (int y = -radius; y <= radius; y++) {
+            int x = (int)sqrt(radius * radius - y * y);  //  x*x + y*y = r*r -> x = sqrt(r*r-y*y)
+            SDL_RenderDrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y);
+        }
+    }
+    void drawFilledCircleBresenham(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+        int x = radius;
+        int y = 0;
+        int p = 1 - radius;
+        // Dış çemberi çiz
+        while (x >= y) {
+            SDL_RenderDrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y); 
+            SDL_RenderDrawLine(renderer, centerX - x, centerY - y, centerX + x, centerY - y); 
+            SDL_RenderDrawLine(renderer, centerX - y, centerY + x, centerX + y, centerY + x); 
+            SDL_RenderDrawLine(renderer, centerX - y, centerY - x, centerX + y, centerY - x);
+            
+            SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+            SDL_RenderDrawPoint(renderer, centerX - x, centerY + y);
+            SDL_RenderDrawPoint(renderer, centerX + x, centerY - y);
+            SDL_RenderDrawPoint(renderer, centerX - x, centerY - y);
+            SDL_RenderDrawPoint(renderer, centerX + y, centerY + x);
+            SDL_RenderDrawPoint(renderer, centerX - y, centerY + x);
+            SDL_RenderDrawPoint(renderer, centerX + y, centerY - x);
+            SDL_RenderDrawPoint(renderer, centerX - y, centerY - x);
+            y++;
+            if (p < 0)
+                p += 2 * y + 1;
+            else {
+                x--;
+                p += 2 * (y - x) + 1;
+            }
+        }
+    }
 }
