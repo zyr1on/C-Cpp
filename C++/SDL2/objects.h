d825153bc2f506238a93216f041126277716f0e9
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-03 05:53:57 +0300

    Update objects.h

--- C++/SDL2/objects.h
+++ C++/SDL2/objects.h
@@ -1,23 +1,44 @@
 #pragma once
 #include"algorithms.h"
 
-enum CircleType{
+enum ObjectType{
     FILLED,
-    NOTFILLED
+    OUTLINE
 };
-template<CircleType type>
+template<ObjectType type>
 class Circle {
 public:
-    int x,y;
-    int radius;
-
+    int x,y,radius;
     Circle(int x, int y, int r) : x(x), y(y), radius(r) {}
-    void draw(SDL_Renderer* ren) 
+    void render(SDL_Renderer* ren) 
     {
         drawCircle(ren, x,y,radius);
     }
 private:
     constexpr static auto drawCircle = (type == FILLED) ? 
         &algorithm::drawFilledCircleBresenham : &algorithm::drawCircleBresenham;
-
 };
+template<ObjectType type>
+class Rectangle {
+public:
+    SDL_Rect rect;
+    Rectangle(int x, int y, int w, int h) : rect{x, y, w, h} {
+        if (type == FILLED)
+            renderType = &Rectangle::fillRender;
+        else
+            renderType = &Rectangle::outlineRender;
+    }
+    void render(SDL_Renderer* renderer) {
+        (this->*renderType)(renderer);
+    }
+private:
+    void (Rectangle::*renderType)(SDL_Renderer*) = nullptr;
+    void fillRender(SDL_Renderer* renderer) {
+        SDL_RenderFillRect(renderer, &rect);
+    }
+    void outlineRender(SDL_Renderer* renderer) {
+        SDL_RenderDrawRect(renderer, &rect);
+    }
+};
+
+
