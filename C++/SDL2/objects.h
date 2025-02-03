44e0aa97ee7bea9b8b30719d28ae5ccc4a6f9e6a
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-03 14:57:25 +0300

    Update objects.h

--- C++/SDL2/objects.h
+++ C++/SDL2/objects.h
@@ -1,6 +1,5 @@
-#pragma once
 #include"algorithms.h"
-
+#pragma once
 enum ObjectType{
     FILLED,
     OUTLINE
@@ -9,7 +8,7 @@ template<ObjectType type>
 class Circle {
 public:
     int x,y,radius;
-    Circle(int x, int y, int r) : x(x), y(y), radius(r) {}
+    Circle(int x, int y, int r) : x(x), y(y), radius(r) {    }
     void render(SDL_Renderer* ren) 
     {
         drawCircle(ren, x,y,radius);
@@ -23,10 +22,12 @@ class Rectangle {
 public:
     SDL_Rect rect;
     Rectangle(int x, int y, int w, int h) : rect{x, y, w, h} {
-        if (type == FILLED)
+        if (type == FILLED) {
             renderType = &Rectangle::fillRender;
-        else
+        }
+        else {
             renderType = &Rectangle::outlineRender;
+        }
     }
     void render(SDL_Renderer* renderer) {
         (this->*renderType)(renderer);
@@ -41,4 +42,51 @@ private:
     }
 };
 
+bool CheckCollisionCircleRect(Circle<FILLED>& c, SDL_Rect& rect) 
+{
+    float closestX, closestY;
+    if(c.x < rect.x) {
+        closestX = rect.x;
+    }
+    else if(c.x > rect.x + rect.w){
+        closestX = rect.x + rect.w;
+    }
+    else {
+        closestX = c.x;
+    }
+    if(c.y < rect.y) {
+        closestY = rect.y;
+    }
+    else if(c.y > rect.y + rect.h){
+        closestY = rect.y + rect.h;
+    }
+    else {
+        closestY = c.y;
+    }
+    float dx = closestX - c.x;
+    float dy = closestY - c.y;
+    return (dx*dx + dy*dy) <= (c.radius*c.radius);
+}
 
+bool CirclePaddleCollisionLeft(Circle<FILLED>&c, SDL_Rect& rect, int& circleSpeedX,  int& circleSpeedY) 
+{
+    if (c.x - c.radius + circleSpeedX <= rect.x + rect.w &&
+            c.y >= rect.y && c.y <= rect.y + rect.h &&
+            circleSpeedX < 0) 
+    {
+            c.x = rect.x + rect.w + c.radius; // Paddle'ın dışına it
+            circleSpeedX *= -1;
+    }
+
+}
+bool CirclePaddleCollisionRight(Circle<FILLED>&c, SDL_Rect& rect, int& circleSpeedX,  int& circleSpeedY) 
+{
+   if (c.x + c.radius + circleSpeedX >= rect.x &&
+            c.y >= rect.y && c.y <= rect.y + rect.h &&
+            circleSpeedX > 0) {
+            
+            c.x = rect.x - c.radius; // Paddle'ın dışına it
+            circleSpeedX *= -1;
+    }
+
+}
