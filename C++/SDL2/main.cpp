7d6110125fee06de0614c6e22a7261a678cef97c
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-03 14:57:37 +0300

    Update main.cpp

--- C++/SDL2/main.cpp
+++ C++/SDL2/main.cpp
@@ -1,5 +1,6 @@
 #include<SDL2/SDL.h>
 #include"objects.h"
+#include<iostream>
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -8,6 +9,9 @@ bool isRunning = false;
 const int FPS = 60;
 const int frameDelay = 1000 / FPS;
 
+
+
+
 int main(int argc, char** argv)
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -15,10 +19,14 @@ int main(int argc, char** argv)
     renderer = SDL_CreateRenderer(window, 0, -1);
     isRunning = true;
 
-    // Circle<FILLED/UNFILLED> ball(xPos,yPos,radius);
     Circle<FILLED> ball(400,300,10);
-    int circleSpeedX = 5;
-    int circleSpeedY = 5;
+    int ballSpeedX = 5;
+    int ballSpeedY = 5;
+
+    SDL_Rect rect1{50,300,50,250};
+    SDL_Rect rect2{800-50,300,50,250}; // rightPaddle
+
+    //Rectangle<FILLED> rect3(400,400,200,55);
 
     Uint32 frameStart, frameTime;
 
@@ -26,27 +34,45 @@ int main(int argc, char** argv)
     {
         frameStart = SDL_GetTicks();
 
-        ball.x += circleSpeedX;
-        ball.y += circleSpeedY;
+        ball.x += ballSpeedX;
+        ball.y += ballSpeedY;
 
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
-                isRunning = false;
+                isRunning = false;            
+        }
+        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+        if (currentKeyStates[SDL_SCANCODE_W])
+            rect1.y -= 5;
+        if (currentKeyStates[SDL_SCANCODE_S])
+            rect1.y += 5;
 
+        if (currentKeyStates[SDL_SCANCODE_UP])
+            rect2.y -= 5;
+        if (currentKeyStates[SDL_SCANCODE_DOWN])
+            rect2.y += 5;
+
+        // bounce
         if(ball.y + ball.radius >= 600 || ball.y - ball.radius <= 0)
-            circleSpeedY *= -1;
-        if(ball.x + ball.radius >= 800 || ball.x - ball.radius <= 0)
-            circleSpeedX *= -1;
-       
+            ballSpeedY *= -1;
+
+
+        CirclePaddleCollisionLeft(ball,rect1,ballSpeedX,ballSpeedY);
+        CirclePaddleCollisionRight(ball,rect2,ballSpeedX,ballSpeedY);
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_RenderFillRect(renderer,&rect1);
+            SDL_RenderFillRect(renderer,&rect2);
+
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-            ball.draw(renderer);
+            ball.render(renderer);
         SDL_RenderPresent(renderer);
 
         frameTime = SDL_GetTicks() - frameStart;
-
+        
         if(frameDelay > frameTime)
             SDL_Delay(frameDelay - frameTime); 
     }
