6bfdea4c7a094c7ad879b78977784f3f8c739d11
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-06 02:37:53 +0300

    Update bouncingSDL2.cpp

--- C++/bouncingBall/bouncingSDL2.cpp
+++ C++/bouncingBall/bouncingSDL2.cpp
@@ -2,7 +2,25 @@
 #include<iostream>
 #include<cmath>
 
-void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius); // polar
+struct Circle 
+{
+    int x,y;
+    int speedX,speedY;
+    int radius;
+    Circle(int x, int y, int speedX, int speedY, int r): 
+    x(x), 
+    y(y), 
+    speedX(speedX),
+    speedY(speedY),
+    radius(r) 
+    {}
+    void draw(SDL_Renderer* renderer) {
+        for (int cy = -radius; cy <= radius; cy++) {
+            int cx = (int)sqrt(radius * radius - cy * cy);  //  x*x + y*y = r*r -> x = sqrt(r*r-y*y)
+            SDL_RenderDrawLine(renderer, x - cx, y + cy, cx + x, cy + y);
+        }
+    }
+};
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -16,46 +34,33 @@ int main(int argc, char** argv)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     isRunning = true;
 
-    int xPos = 400;
-    int yPos = 300;
-    int radius = 20;
-    int ballSpeedX = -3;
-    int ballSpeedY = -3;
-
+    Circle ball(400,300,-3,-3,25);
+    
     while (isRunning)
     {
-        xPos += ballSpeedX;
-        yPos += ballSpeedY;
-        
+        ball.x += ball.speedX;
+        ball.y += ball.speedY;
+
         while (SDL_PollEvent(&event))
             if (event.type == SDL_QUIT)
                 isRunning = false;
         // bounce
-        if(yPos +  radius >= 600 || yPos - radius <= 0)
-            ballSpeedY *= -1;
-        if(xPos + radius >= 800 || xPos - radius <= 0)
-            ballSpeedX *= -1;
+        if(ball.y + ball.radius >= 600 || ball.y - ball.radius <= 0)
+            ball.speedY *= -1;
+        if(ball.x + ball.radius >= 800 || ball.x - ball.radius <= 0)
+            ball.speedX *= -1;
 
         SDL_SetRenderDrawColor(renderer, 51, 77, 77, 255);
         SDL_RenderClear(renderer);
-
+    
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-            drawCircle(renderer,xPos,yPos,25);
-        
-        SDL_RenderPresent(renderer);   
+            ball.draw(renderer);
+        SDL_RenderPresent(renderer);
+		
     }
-
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
-}
-
-void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius)
-{
-    for (int y = -radius; y <= radius; y++) {
-        int x = (int)sqrt(radius * radius - y * y);  //  x*x + y*y = r*r -> x = sqrt(r*r-y*y)
-        SDL_RenderDrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y);
-    }
 }
