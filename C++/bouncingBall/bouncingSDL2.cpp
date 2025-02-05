2d59f8ba5b2a4a50b187bd7f0fe3c687f0cf2ccd
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-06 02:46:19 +0300

    Update bouncingSDL2.cpp

--- C++/bouncingBall/bouncingSDL2.cpp
+++ C++/bouncingBall/bouncingSDL2.cpp
@@ -2,11 +2,9 @@
 #include<iostream>
 #include<cmath>
 
-struct Circle 
-{
-    int x,y;
-    int speedX,speedY;
-    int radius;
+struct Circle {
+public:
+    int x,y,speedX,speedY,radius;
     Circle(int x, int y, int speedX, int speedY, int r): 
     x(x), 
     y(y), 
@@ -26,16 +24,18 @@ SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Event event;
 bool isRunning = false;
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main(int argc, char** argv)
 {
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("SDL Circle Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("SDL Circle Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     isRunning = true;
 
-    Circle ball(400,300,-3,-3,25);
-    
+    Circle ball(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2,-3,-3,25);
+
     while (isRunning)
     {
         ball.x += ball.speedX;
@@ -44,23 +44,24 @@ int main(int argc, char** argv)
         while (SDL_PollEvent(&event))
             if (event.type == SDL_QUIT)
                 isRunning = false;
+    
         // bounce
-        if(ball.y + ball.radius >= 600 || ball.y - ball.radius <= 0)
+        if(ball.y + ball.radius >= SCREEN_HEIGHT || ball.y - ball.radius <= 0)
             ball.speedY *= -1;
-        if(ball.x + ball.radius >= 800 || ball.x - ball.radius <= 0)
+        if(ball.x + ball.radius >= SCREEN_WIDTH || ball.x - ball.radius <= 0)
             ball.speedX *= -1;
 
         SDL_SetRenderDrawColor(renderer, 51, 77, 77, 255);
         SDL_RenderClear(renderer);
-    
+        
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
             ball.draw(renderer);
         SDL_RenderPresent(renderer);
-		
+
     }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
