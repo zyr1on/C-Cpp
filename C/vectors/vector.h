1ce837e983fcd002471670e20f19dce1a7bc3e04
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-02-07 05:55:49 +0300

    Update vector.h

--- C/vectors/vector.h
+++ C/vectors/vector.h
@@ -1,109 +1,116 @@
 /*
-	AUTHOR: Semih "zyron" Özdemir
- 	ordered dynamic array implementation of C
+	AUTHOR: Semih "zyr1on" Özdemir
+ 	unordered dynamic array implementation of C
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include<string.h>
+
 #define INITIAL_CAP 5
 typedef struct {
     int* m_data;
     size_t m_size;
     size_t m_capacity;
     int m_initialized;
-} ovector;
+} vector;
 
-int ordered_validate_vector(ovector* v, char* _errStr) {
-    if(v == NULL || v->m_data == NULL || v->m_initialized != -1) 
-    {
+int compare(const void* a, const void* b) {
+   return (*(int*)a - *(int*)b);
+}
+int validate_vector(vector* v, char* _errStr) {
+    if(v == NULL || v->m_data == NULL || v->m_initialized != -1) {
         fprintf(stderr,_errStr);
         return 0;
     }
     return 1;
 }
 
-int ovector_init(ovector*v) {
+int vector_init(vector*v) {
     if(v->m_initialized == -1) {
-        fprintf(stderr, "ovector_init: Vector is already m_initialized\n");
+        fprintf(stderr, "vector_init: Vector is already initialized\n");
         return -1;
     }
-    v->m_data = (int*)malloc(sizeof(int)*INITIAL_CAP); // 4byte;
+    v->m_data = (int*)malloc(sizeof(int)*INITIAL_CAP);
     if(v->m_data == NULL) {
-        perror("ovector_init: Memory allocation error\n");
+        perror("vector_init: Memory allocation error\n");
         return -1;
     }
     v->m_size = 0;
     v->m_capacity = INITIAL_CAP;
     v->m_initialized = -1;
-    return 0;
+    return 0;  
 }
 
-int ovector_IndexAt(ovector* v, int element, int type) {
-    if(!ordered_validate_vector(v,"ovector_IndexAt: Vector is NULL or not m_initialized\n")) return -1;
-    int left = 0, right = v->m_size - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (v->m_data[mid] == element) return mid;
-        else if (v->m_data[mid] > element) right = mid - 1;
-        else left = mid + 1;
-    }
-    if(type == 0) {
-        fprintf(stderr, "ovector_IndexAt: Element not found.\n");
-        return -1;
-    }
-    else return left;
-}
-
-// int ovector_insert(ovector* v, int element) {
-//     if(!ordered_validate_vector(v,"ovector_insert: Vector is NULL or not m_initialized\n")) return -1;
-//     if (v->m_size >= v->m_capacity) {
-//         v->m_capacity *= 2;
+// int vector_push_back(vector*v, int element) {
+//     if(!validate_vector(v,"vector_push_back: Vector is NULL or not initialized\n")) return -1;
+//     if(v->m_size == v->m_capacity) {
+//         v->m_capacity *=2;
 //         int* temp = (int*)malloc(sizeof(int) * v->m_capacity);
-//         if (temp == NULL) {
-//             perror("ovector_insert: Temporary memory allocation error");
+//         if(temp == NULL) {
+//             perror("vector_push_back: Temporary Memory allocation error");
 //             return -1;
 //         }
 //         memcpy(temp,v->m_data,sizeof(int)*v->m_size);
 //         free(v->m_data);
 //         v->m_data = temp;
 //     }
-//     int index = ovector_IndexAt(v, element,1);
-//     if (index < v->m_size)
-//         memmove(v->m_data + index + 1, v->m_data + index, sizeof(int) * (v->m_size - index));
-//     v->m_data[index] = element;
-//     v->m_size++;
+//     v->m_data[v->m_size++] = element;
 //     return 0;
 // }
 
-int ovector_insert(ovector* v, int element) {
-    if (!ordered_validate_vector(v, "ovector_insert: Vector is NULL or not m_initialized\n"))
-        return -1;
-    if (v->m_size >= v->m_capacity) {
-        int new_m_capacity = v->m_capacity ? v->m_capacity * 2 : 1;
-        int* temp = (int*)realloc(v->m_data, sizeof(int) * new_m_capacity);
+int vector_push_back(vector* v, int element) {
+    if (!validate_vector(v, "vector_push_back: Vector is NULL or not initialized\n")) return -1;
+    if (v->m_size == v->m_capacity) {
+        v->m_capacity *= 2; // Kapasiteyi iki katına çıkarıyoruz
+        int* temp = (int*)realloc(v->m_data, sizeof(int) * v->m_capacity);
         if (temp == NULL) {
-            perror("ovector_insert: Memory allocation error");
+            perror("vector_push_back: Temporary Memory allocation error");
             return -1;
         }
         v->m_data = temp;
-        v->m_capacity = new_m_capacity;
     }
-    int index = ovector_IndexAt(v, element, 1);
-    if (index < v->m_size) {
-        memmove(v->m_data + index + 1, v->m_data + index, sizeof(int) * (v->m_size - index));
+    v->m_data[v->m_size++] = element;
+    return 0;
+}
+
+int vector_push_front(vector* v,int element) {
+    if(!validate_vector(v,"vector_push_front: Vector is NULL or not initialized\n")) return -1;    
+    if(v->m_size >= v->m_capacity) {
+        v->m_capacity *=2;
+        int* temp = (int*)malloc(sizeof(int) * v->m_capacity);
+        if(temp == NULL) {
+            perror("vector_push_back: Temporary Memory allocation error");
+            return -1;
+        }
+        memmove(temp+1,v->m_data,sizeof(int)*v->m_size);
+        free(v->m_data);
+        v->m_data = temp;
     }
-    v->m_data[index] = element;
+    else 
+        memmove(v->m_data + 1,v->m_data,sizeof(int) * (v->m_size));
+    v->m_data[0] = element;
     v->m_size++;
     return 0;
 }
 
-int ovector_delete(ovector*v , int element) 
-{
-    if(!ordered_validate_vector(v,"ovector_delete: Vector is NULL or not m_initialized\n")) return -1;
-    int index = ovector_IndexAt(v,element,0);
-    if(index == -1) 
+int vector_IndexAt(vector* v, int element)  {
+    if(!validate_vector(v,"vector_IndexAt: Vector is empty or NULL | maybe not initialized\n")) return -1;  
+    for(int i=0;i<v->m_size;i++)
+        if(v->m_data[i] == element) return i;
+    return -1;
+}
+
+int vector_delete(vector* v, int element) {
+    if (v->m_data == NULL || v->m_size == 0 || v->m_initialized != 1) {
+        fprintf(stderr, "vector_delete: Vector is empty or NULL | maybe not initialized\n");
+        return -1;
+    }
+    if(!validate_vector(v,"vector_delete: Vector is empty or NULL | maybe not initialized\n")) return -1;  
+    int index = vector_IndexAt(v,element);
+    if(index == -1)
     {
-        fprintf(stderr, "ovector_delete: Element not found\n");
+        fprintf(stderr,"vector_delete: Element not found\n");
         return -1;
     }
     if(index == v->m_size -1) 
@@ -116,29 +123,33 @@ int ovector_delete(ovector*v , int element)
     return 0;
 }
 
-int ovector_pop(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_pop: Vector is NULL or not m_initialized\n")) return -1;
-    return v->m_data[v->m_size--];
+int vector_sort(vector* v) {
+    if(!validate_vector(v,"vector_sort: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    qsort(v->m_data,v->m_size,sizeof(int),compare);
+    return 0;
 }
 
-int ovector_multiply(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_multiply: Vector is NULL or not m_initialized\n")) return -1;
+int vector_pop(vector* v) {
+    if(!validate_vector(v,"vector_pop: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    if(v->m_size > 0) return v->m_data[v->m_size--];
+}
+
+int vector_multiply(vector* v) {
+    if(!validate_vector(v,"vector_multiply: Vector is empty or NULL | maybe not initialized\n")) return -1; 
     int result = 1;
-    for(int i=0;i<v->m_size;i++)
-        result *= v->m_data[i];
+    for(int i=0;i<v->m_size;i++) result *= v->m_data[i];
     return result;
 }
 
-int ovector_sum(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_sum: Vector is NULL or not m_initialized\n")) return -1;
+int vector_sum(vector* v) {
+    if(!validate_vector(v,"vector_sum: Vector is empty or NULL | maybe not initialized\n")) return -1; 
     int sum = 0;
     for(int i=0;i<v->m_size;i++)
         sum += v->m_data[i];
     return sum;
 }
-
-float ovector_avg(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_avg: Vector is NULL or not m_initialized\n")) return -1;
+float vector_avg(vector* v) {
+    if(!validate_vector(v,"vector_avg: Vector is empty or NULL | maybe not initialized\n")) return -1; 
     int sum = 0;
     for(int i=0;i<v->m_size;i++) {
         sum += v->m_data[i];
@@ -146,45 +157,85 @@ float ovector_avg(ovector* v) {
     return (float) ((float)sum/(float)v->m_size);
 }
 
-int ovector_back(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_last: Vector is NULL or not m_initialized\n")) return -1;
-    return v->m_data[v->m_size-1];   
+int vector_front(vector* v) {
+    if(!validate_vector(v,"vector_front: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    return v->m_data[0];
 }
-int ovector_front(ovector* v) {
-    if(!ordered_validate_vector(v,"ovector_front: Vector is NULL or not m_initialized\n")) return -1;
-    return v->m_data[0];   
+int vector_back(vector* v) {
+    if(!validate_vector(v,"vector_back: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    return v->m_data[v->m_size-1];
 }
 
-int ovector_m_size(ovector *v) {
-	if(!ordered_validate_vector(v,"ovector_m_size: Vector is NULL or not m_initialized\n")) return -1;
+int vector_max(vector* v) {
+    if(!validate_vector(v,"vector_max: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    int max = v->m_data[0];
+    for(int i=1;i<v->m_size;i++)
+        if(v->m_data[i] > max) 
+            max = v->m_data[i];
+    return max;
+}
+
+int vector_min(vector* v) {
+    if(!validate_vector(v,"vector_min: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    int min = v->m_data[0];
+    for(int i=1;i<v->m_size;i++)
+        if(v->m_data[i] < min)
+            min = v->m_data[i];
+    return min;
+}
+
+int vector_m_size(vector *v) {
+	if(!validate_vector(v,"vector_m_size: Vector is empty or NULL | maybe not initialized\n")) return -1; 
     return v->m_size;
 }
 
-void ovector_print(ovector*v) {
-	if(!ordered_validate_vector(v,"ovector_print: Vector is NULL or not m_initialized\n")) return;
+void vector_print(vector*v) {
+    if(!validate_vector(v,"vector_print: Vector is empty or NULL | maybe not initialized\n")) return; 
     printf("[");
     for(int i=0;i<v->m_size-1;i++) printf("%d,",v->m_data[i]);
     printf("%d]\n",v->m_data[v->m_size-1]);
     return;
 }
 
-int ovector_destroy(ovector*v){
-    if(!ordered_validate_vector(v,"ovector_destroy: Vector is NULL or not m_initialized\n")) return -1;
-    free(v->m_data);
-    v->m_data = NULL;
-    v->m_size = 0;
+void vector_shuffle(vector* v) {
+    if(!validate_vector(v,"vector_shuffle: Vector is empty or NULL | maybe not initialized\n")) return; 
+    srand(time(0));
+    for(int i=0;i<v->m_size;i++) {
+        int randNum = rand() % v->m_size;
+        int temp = v->m_data[i];
+        v->m_data[i] = v->m_data[randNum];
+        v->m_data[randNum] = temp;
+    }
+}
+int vector_reverse(vector* v) {
+    if(!validate_vector(v,"vector_reverse: Vector is empty or NULL | maybe not initialized\n")) return -1; 
+    for(int i=0;i<v->m_size/2;i++) 
+    {
+        int temp = v->m_data[i];
+        v->m_data[i] = v->m_data[v->m_size -1-i];
+        v->m_data[v->m_size-1-i] = temp;
+    }
     return 0;
 }
 
-void ovector_fill(ovector* v, int fill_val) 
-{
-    if(!ordered_validate_vector(v,"ovector_fill: Vector is NULL or not m_initialized\n")) return;
+int vector_destroy(vector*v) {
+    if(!validate_vector(v,"vector_destroy: Vector is NULL | maybe already destroyed or not initialized\n")) return -1;
+    free(v->m_data);
+    v->m_data = NULL;
+    v->m_size = 0;
+    v->m_capacity = 0;
+    v->m_initialized = 0;
+    return 0;
+}
+
+void vector_fill(vector* v, int fill_val) {
+    if(!validate_vector(v,"vector_fill: Vector is empty or NULL | maybe not initialized\n")) return; 
     memset(v->m_data,fill_val,sizeof(int)*v->m_size);
 }
 
-ovector ovector_trim(ovector* v, int _startIndex, int _endIndex) {
-    ovector temp;
-    ovector_init(&temp); 
+vector vector_trim(vector* v, int _startIndex, int _endIndex) {
+    vector temp;
+    vector_init(&temp); 
     temp.m_size = _endIndex - _startIndex;
     memcpy(temp.m_data,v->m_data + _startIndex, sizeof(int) * temp.m_size);
     return temp;    
