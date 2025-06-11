2b8c461d765c719bf6dbce76744e7952727dc0da
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-06-11 12:23:06 +0300

    Create example.c

---
+++ C/vectors/example.c
@@ -0,0 +1,28 @@
+#include "CVector.h"
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+}vec3;
+
+int main(int argc, char const *argv[])
+{
+    vector(vec3) container;
+    vector_init(container);
+
+    vec3 a = {3.2,2.1, 33.59};
+    vec3 b = {0.0,0.0,0.0};
+
+    vector_push_back(container,a);
+    vector_push_back(container,b);
+    vector_push_back_args(container,{1,2,3},{1,2,4},{1,2,32});
+
+    vector_foreach(container,p){
+        printf("(%.2f, %.2f, %.2f)\n", p->x,p->y, p->z);
+    }
+
+    vector_destroy(container);
+    
+    return 0;
+}
